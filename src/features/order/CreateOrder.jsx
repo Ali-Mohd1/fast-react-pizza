a3420e7f6a709cc7636a472a60fab927a3ae5615
Ali-Mohd1 <ali.beigh123@gmail.com>
2025-01-24 12:13:55 +0530

    first commit

---
+++ src/features/order/CreateOrder.jsx
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+import { useSelector } from "react-redux";
+
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
+
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetale",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
+
+function CreateOrder() {
+  // const [withPriority, setWithPriority] = useState(false);
+  const username = useSelector((state) => state.user.username);
+  const navigation = useNavigation();
+  const isSubmiting = navigation.state === "submitting";
+  const formErrors = useActionData();
+
+  const cart = fakeCart;
+
+  return (
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+      <Form method="post">
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input
+            type="text"
+            className="input grow"
+            name="customer"
+            defaultValue={username}
+            required
+          />
+        </div>
+
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input type="tel" className="input w-full" name="phone" required />
+          </div>
+          {formErrors?.phone && (
+            <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+              {formErrors.phone}
+            </p>
+          )}
+        </div>
+
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              className="input w-full"
+              name="address"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="mb-12 flex items-center gap-5">
+          <input
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
+            type="checkbox"
+            name="priority"
+            id="priority"
+            // value={withPriority}
+            // onChange={(e) => setWithPriority(e.target.checked)}
+          />
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
+        </div>
+
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button
+            disabled={isSubmiting}
+            className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block rounded-full tracking-wider"
+          >
+            {isSubmiting ? "Placing Order..." : "Order now"}
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+
+  const newOrder = await createOrder(order);
+
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone =
+      "Please give use your correct phone number. we might need it to contact you.";
+  }
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay, create new order and redirect
+  return redirect(`/order/${newOrder.id}`);
+};
+
+export default CreateOrder;
